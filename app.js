fd78ec249b02e697fd3e54a84eae33efab33d848
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-03 10:52:47 +0700

    adding : schema and model for GET and POST products data

--- app.js
+++ app.js
@@ -18,20 +18,48 @@ mongoose.connect(process.env.CONNECTION_STRING)
 })
 .catch((err) => {
 	console.log(err);
-})
+});
+
+// product model
+const productSchema = mongoose.Schema({
+	name: String,
+	image: String,
+	countInStock: {
+		type: Number,
+		required: true
+	} ,
+});
+
+const Product = mongoose.model('product', productSchema);
+
 // initial route
-app.get(`${api}/products`, (req, res) => {
-	const product = {
-		id: 1,
-		name: 'Laptop Macbook',
-		image: 'some_url', 
+app.get(`${api}/products`, async (req, res) => {
+	const productList = await Product.find();
+
+	if(!productList) {
+		res.status(500).json({success: false})
 	}
-	res.send(product);
+
+	res.send(productList);
 });
 
 app.post(`${api}/products`, (req, res) => {
-	const newProduct = req.body;
-	res.send(newProduct);
+	// mengambil req body dari front-end
+	const product = new Product({
+		name: req.body.name,
+		image: req.body.image,
+		countInStock: req.body.countInStock
+	});
+
+	// kembalikan promise resolve dan reject ke front end sebagai respon
+	product.save().then((createdProduct) => {
+		res.status(201).json(createdProduct)
+	}).catch((err) => {
+		res.status(500).json({
+			error: err,
+			success: false
+		})
+	})
 });
 
 // initial server : start awal respon server running
