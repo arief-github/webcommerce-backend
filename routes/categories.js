c5263a8363c2cc6a186ea4f01fbd7cba865ac92a
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-10 18:34:07 +0700

    add : GET, POST, and DELETE method to Categories

---
+++ routes/categories.js
@@ -0,0 +1,43 @@
+const { Category } = require('../models/category');
+const express = require('express');
+const router = express.Router();
+
+router.get(`/`, async (req, res) => {
+    const categoryList = await Category.find();
+
+    if (!categoryList) {
+        res.status(500).json({ success: false })
+    }
+
+    res.send(categoryList);
+});
+
+router.post(`/`, async (req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    });
+
+    category = await category.save();
+
+    if (!category) {
+        return res.status(404).send('the category cannot be created')
+    }
+
+    res.send(category);
+});
+
+router.delete(`/:id`, async (req, res) => {
+    Category.findByIdAndRemove(req.params.id).then((category) => {
+    	if(category) {
+    		return res.status(200).json({ success: true, message: 'the category is delete' })
+    	} else {
+    		return res.status(404).json({ success: false, message: 'category not found' })
+    	}
+    }).catch((err) => {
+    	return res.status(400).json({success: false, error: err})
+    })
+})
+
+module.exports = router;
