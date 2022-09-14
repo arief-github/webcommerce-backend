15ae4f694a75b1945de052b2931fcfd0c63a5ba0
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-14 23:26:28 +0700

    refactor : POST method from Products

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,5 @@
 const { Product } = require('../models/product');
+const { Category } = require('../models/category');
 const express = require('express');
 const router = express.Router();
 
@@ -12,23 +13,30 @@ router.get(`/`, async (req, res) => {
     res.send(productList);
 });
 
-router.post(`/`, (req, res) => {
-    // mengambil req body dari front-end
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    });
+router.post(`/`, async (req, res) =>{
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category')
 
-    // kembalikan promise resolve dan reject ke front end sebagai respon
-    product.save().then((createdProduct) => {
-        res.status(201).json(createdProduct)
-    }).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
+    let product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
     })
-});
+
+    product = await product.save();
+
+    if(!product) 
+    return res.status(500).send('The product cannot be created')
+
+    res.send(product);
+})
 
 module.exports = router;
