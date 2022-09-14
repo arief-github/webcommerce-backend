f59cab3a546da26df1379a8e977b0e6bed77da65
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-14 22:20:45 +0700

    add: PUT by ID method to Categories

--- routes/categories.js
+++ routes/categories.js
@@ -12,10 +12,10 @@ router.get(`/`, async (req, res) => {
     res.status(200).send(categoryList);
 });
 
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
     const category = await Category.findById(req.params.id);
 
-    if(!category) {
+    if (!category) {
         res.status(500).json({ message: 'The category with the given ID was removed' });
     }
 
@@ -38,15 +38,32 @@ router.post(`/`, async (req, res) => {
     res.send(category);
 });
 
+router.put('/:id',async (req, res)=> {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon || category.icon,
+            color: req.body.color,
+        },
+        { new: true}
+    )
+
+    if(!category)
+    return res.status(400).send('the category cannot be created!')
+
+    res.send(category);
+})
+
 router.delete(`/:id`, async (req, res) => {
     Category.findByIdAndRemove(req.params.id).then((category) => {
-    	if(category) {
-    		return res.status(200).json({ success: true, message: 'the category is delete' })
-    	} else {
-    		return res.status(404).json({ success: false, message: 'category not found' })
-    	}
+        if (category) {
+            return res.status(200).json({ success: true, message: 'the category is delete' })
+        } else {
+            return res.status(404).json({ success: false, message: 'category not found' })
+        }
     }).catch((err) => {
-    	return res.status(400).json({success: false, error: err})
+        return res.status(400).json({ success: false, error: err })
     })
 })
 
