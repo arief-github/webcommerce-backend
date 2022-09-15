bc2d86080468905cd98765750d3ac64fd6ed836f
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-15 17:54:22 +0700

    add : PUT by ID method to Products

--- routes/products.js
+++ routes/products.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-     // show product with complete information
+    // show product with complete information
     // const productList = await Product.find();
 
     // show product just showing name, image. but id is removed.
@@ -20,19 +20,19 @@ router.get(`/`, async (req, res) => {
     res.send(productList);
 });
 
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
     const product = await Product.findById(req.params.id).populate('category');
 
-    if(!product) {
-        res.status(500).json({success: false});
+    if (!product) {
+        res.status(500).json({ success: false });
     }
 
     res.send(product);
 })
 
-router.post(`/`, async (req, res) =>{
+router.post(`/`, async (req, res) => {
     const category = await Category.findById(req.body.category);
-    if(!category) return res.status(400).send('Invalid Category')
+    if (!category) return res.status(400).send('Invalid Category')
 
     let product = new Product({
         name: req.body.name,
@@ -50,8 +50,34 @@ router.post(`/`, async (req, res) =>{
 
     product = await product.save();
 
-    if(!product) 
-    return res.status(500).send('The product cannot be created')
+    if (!product)
+        return res.status(500).send('The product cannot be created')
+
+    res.send(product);
+})
+
+router.put('/:id', async (req, res) => {
+    const category = await Category.findById(req.body.category);
+    if (!category) return res.status(400).send('Invalid Category')
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id, {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+        }, { new: true }
+    )
+
+    if (!product)
+        return res.status(500).send('the category cannot be updated!')
 
     res.send(product);
 })
