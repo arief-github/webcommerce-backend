56738d00ea6c63bd89ecf610b45cd4693219812c
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-15 18:53:17 +0700

    add : DELETE by ID method to Products

--- routes/products.js
+++ routes/products.js
@@ -82,4 +82,16 @@ router.put('/:id', async (req, res) => {
     res.send(product);
 })
 
+router.delete(`/:id`, async (req, res) => {
+    Product.findByIdAndRemove(req.params.id).then((product) => {
+        if (product) {
+            return res.status(200).json({ success: true, message: 'the product is delete' })
+        } else {
+            return res.status(404).json({ success: false, message: 'product not found' })
+        }
+    }).catch((err) => {
+        return res.status(500).json({ success: false, error: err })
+    })
+})
+
 module.exports = router;
