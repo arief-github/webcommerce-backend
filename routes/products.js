1557f254983dadecfe5908567fff24e59d324012
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-15 17:38:25 +0700

    add: Populate method to Product API

--- routes/products.js
+++ routes/products.js
@@ -8,7 +8,10 @@ router.get(`/`, async (req, res) => {
     // const productList = await Product.find();
 
     // show product just showing name, image. but id is removed.
-    const productList = await Product.find().select('name image -_id');
+    // const productList = await Product.find().select('name image -_id');
+
+    // populate product to category reference
+    const productList = await Product.find().populate('category');
 
     if (!productList) {
         res.status(500).json({ success: false })
@@ -18,7 +21,7 @@ router.get(`/`, async (req, res) => {
 });
 
 router.get('/:id', async(req, res) => {
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id).populate('category');
 
     if(!product) {
         res.status(500).json({success: false});
