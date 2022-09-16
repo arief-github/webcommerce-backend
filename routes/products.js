5a581cf14d6be20d7f859e70e778d44aaf498a5e
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-16 18:56:41 +0700

    add : GET by Featured and also add Limit Count

--- routes/products.js
+++ routes/products.js
@@ -109,6 +109,17 @@ router.get(`/get/count`, async (req, res) =>{
     res.send({
         productCount: productCount
     });
+});
+
+router.get(`/get/featured/:count`, async(req, res) => {
+    const count = req.params.count ? req.params.count : 0;
+    const products = await Product.find({ isFeatured: true }).limit(+count);
+
+    if(!products) {
+        res.status(500).json({ success: false });
+    }
+
+    res.send(products);
 })
 
 module.exports = router;
