b611234b477c325246a82b7e97e33dbc452d4ce9
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-17 15:44:59 +0700

    install and implement bcrypt; add : GET by ID method

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 router.get('/', async(req, res) => {
-	const userList = await User.find();
+	// menghilangkan password hash
+	const userList = await User.find().select('name phone email')
 
 	if(!userList) {
 		res.status(500).json({ success: false })
@@ -12,11 +14,21 @@ router.get('/', async(req, res) => {
 	res.send(userList);
 });
 
+router.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id).select('-passwordHash');
+
+    if (!user) {
+        res.status(500).json({ success: false });
+    }
+
+    res.send(user);
+});
+
 router.post('/', async(req, res) => {
 	let user = new User({
 		name: req.body.name,
 		email: req.body.email,
-		passwordHash: req.body.passwordHash,
+		passwordHash:  bcrypt.hashSync(req.body.password, 10),
 		phone: req.body.phone,
 		isAdmin: req.body.isAdmin,
 		apartment: req.body.apartment,
