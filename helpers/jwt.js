547458c04011800e9737f624098c7e77e5e08633
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-20 16:57:42 +0700

    give authorization to GET products and feature product

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,17 +1,18 @@
 const expressJwt = require("express-jwt");
 
-function authJwt(){
-	const secret = process.env.secret;
-	const api = process.env.API_URL;
-	return expressJwt({
-		secret,
-		algorithms: ['HS256']
-	}).unless({
-		path: [
-			`${api}/users/login`,
-			`${api}/users/register`
-		]
-	})
+function authJwt() {
+    const secret = process.env.secret;
+    const api = process.env.API_URL;
+    return expressJwt({
+        secret,
+        algorithms: ['HS256']
+    }).unless({
+        path: [
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            `${api}/users/login`,
+            `${api}/users/register`
+        ]
+    })
 }
 
 module.exports = authJwt;
