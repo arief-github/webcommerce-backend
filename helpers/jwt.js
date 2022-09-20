b8a62ba0f0e09798a9b8d14bd757fe64f88d3c5a
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-20 16:46:47 +0700

    give authorization to login and register process

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,9 +2,15 @@ const expressJwt = require("express-jwt");
 
 function authJwt(){
 	const secret = process.env.secret;
+	const api = process.env.API_URL;
 	return expressJwt({
 		secret,
 		algorithms: ['HS256']
+	}).unless({
+		path: [
+			`${api}/users/login`,
+			`${api}/users/register`
+		]
 	})
 }
 
--- routes/users.js
+++ routes/users.js
@@ -72,6 +72,27 @@ router.post('/login', async (req, res) => {
     } else {
         res.status(400).send('password is wrong!');
     }
+});
+
+router.post('/register', async (req,res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
+
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
 })
 
 module.exports = router;
