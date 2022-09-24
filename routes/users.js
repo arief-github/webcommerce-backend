8cc8ad58567653c34a23d7e8aadd057f834f04ca
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-24 09:23:25 +0700

    add : DELETE and PUT method for Users

--- routes/users.js
+++ routes/users.js
@@ -46,6 +46,41 @@ router.post('/', async (req, res) => {
     res.send(user);
 });
 
+router.put('/:id', async(req, res) => {
+    const userExist = await User.findById(req.params.id);
+
+    let newPassword;
+
+    if(req.body.password) {
+        newPassword = bcrypt.hashSync(req.body.password, 10);
+    } else {
+        newPassword = userExist.passwordHash;
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: newPassword,
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            street: req.body.street,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            country: req.body.country,
+        },
+        { new: true}
+    )
+
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
+    
+    res.send(user);
+})
+
+
 router.post('/login', async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     const secret = process.env.secret;
@@ -95,5 +130,29 @@ router.post('/register', async (req,res)=>{
 
     res.send(user);
 })
+
+router.delete(`/:id`, async (req, res) => {
+    User.findByIdAndRemove(req.params.id).then((user) => {
+        if (user) {
+            return res.status(200).json({ success: true, message: 'the user is delete' })
+        } else {
+            return res.status(404).json({ success: false, message: 'user not found' })
+        }
+    }).catch((err) => {
+        return res.status(500).json({ success: false, error: err })
+    })
+});
+
+
+router.get(`/get/count`, async (req, res) =>{
+    const userCount = await User.countDocuments();
+
+    if(!userCount) {
+        res.status(500).json({success: false})
+    } 
+    res.send({
+        userCount: userCount
+    });
+});
 
 module.exports = router;
