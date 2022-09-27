824ec2fe4aed9e88f6f217cb28bbd94b54c236b1
github.arief <78076941+arief-github@users.noreply.github.com>
2022-09-27 16:19:21 +0700

    add : GET and GET by ID method for Orders

--- routes/orders.js
+++ routes/orders.js
@@ -3,6 +3,34 @@ const { OrderItem } = require('../models/order-items');
 const express = require('express');
 const router = express.Router();
 
+
+router.get('/', async (req, res) => {
+    const orderList = await Order.find().populate('user', 'name').sort({'dateOrdered': -1});
+
+    if (!orderList) {
+        res.status(500).json({ success: false })
+    }
+
+    res.send(orderList);
+});
+
+router.get('/:id', async (req, res) => {
+    const order = await Order.findById(req.params.id)
+        .populate('user', 'name')
+        .populate({
+            path: 'orderItems',
+            populate: {
+                path: 'product',
+                populate: 'category'
+            }
+        });
+
+        if(!order) {
+            res.status(500).json({ success: false })
+        }
+        res.send(order);
+});
+
 router.post('/', async (req, res) => {
     const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem) => {
         let newOrderItem = new OrderItem({
@@ -31,10 +59,10 @@ router.post('/', async (req, res) => {
 
     order = await order.save();
 
-    if(!order)
-    return res.status(400).send('the order cannot be created!');
-	
-	res.send(order);
+    if (!order)
+        return res.status(400).send('the order cannot be created!');
+
+    res.send(order);
 });
 
 module.exports = router;
