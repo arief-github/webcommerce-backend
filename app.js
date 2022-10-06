273c0825db199ade0cd02038212f7668c8d95b9a
github.arief <78076941+arief-github@users.noreply.github.com>
2022-10-06 17:28:17 +0700

    setting middleware to access image on the URL without check fot auth

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const api = process.env.API_URL;
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 app.use(errorHandler);
 
 // connect to mongoDB
